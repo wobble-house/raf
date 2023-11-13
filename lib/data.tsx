fd8997f38d9dac04673a1ef4684f333580fa8bd8
wobble-house <ross@wobble.house>
2023-11-13 11:20:43 -0500

    first commit

---
+++ lib/data.tsx
@@ -0,0 +1,62 @@
+export const projects = [
+    {
+        title: "A2IM", 
+        url: "https://www.a2im.org", 
+        type: "front-end", 
+        details: "The American Association of Independent Music (A2IM) is a 501(c)(6) not-for-profit trade organization that serves as a central voice for a diverse community of over 600 independently owned record labels operating within the United States. A2IM  represents their interests in the marketplace, in the media, on Capitol Hill, and as part of the global music community.", 
+        image: {
+            src:'/images/A2IM.png',
+            alt:'home screen of a2im.org'
+        }
+    },
+    {
+        title: "Libera Awards", 
+        url: "https://www.liberaawards.com", 
+        type: 'full-stack', 
+        details: "U.S. based independent music awards site to provide updates each year on the status of each year&apos;s community voting process, ticketing info, historical archives of previous winners, and a simple contact form.",
+        image: {
+            src:'/images/LiberaAwards.png',
+            alt:'home screen of LiberaAwards.com'
+        }
+    },
+    {
+        title: "Indie Week", 
+        url: "https://www.a2imindieweek.org", 
+        type: "full-stack", 
+        details: "An informational website built for an international music business conference.  You can find ticketing/lodging information, program scheduling, and sponsorship information.",
+        image: {
+            src:'/images/IndieWeek.png',
+            alt:'home screen of a2imindieweek.org'
+        }
+    },
+    {
+        title: "Knowledge-base", 
+        url: "https://knowledge-base.a2im.org", 
+        type: "full-stack", 
+        details: "A knowledge-base website created for internal documentation & training used at The American Association of Independent Music (A2IM).  This site features full authentication protection at the domain level.",
+        image: {
+            src:'/images/KnowledgeBase.png',
+            alt:'home screen of a2im&apos;s Knowledge Base'
+        }
+    },
+    {
+        title: "Headless CMS", 
+        url: null, 
+        type: "back-end", 
+        details: "A headless CMS built for A2IM to supply media content to all web properties.  This server is built with AWS services such as EC2, DynamoDB, & S3.  The underlying framework used to build the server is Strapi.  Strapi is capable of  utilizing both RESTful & GraphQL API simultaneously.",
+        image: {
+            src:'/images/HeadlessCMS.png',
+            alt:'home screen of A2IM&apos;s Headless CMS'
+        }
+    },
+    {
+        title: "Community Connections", 
+        url: "https://www.ccfil.com/", 
+        type: "full-stack", 
+        details: "Community Connections provides a comprehensive, semi-independent community-based living program for individuals affected by traumatic brain injury. I have been tasked to modernize their web infrastructure & create new features that integrate into their existing systems",
+        image: {
+            src:'/images/CCFIL.png',
+            alt:'home screen of ccfil.com'
+        }
+    }
+  ]
